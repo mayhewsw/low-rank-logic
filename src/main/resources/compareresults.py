fcc8c581262baa6250b78012a55e4b13f94facf9
Stephen Mayhew <mayhew2@illinois.edu>
2015-02-23 12:04:24 -0600

    was missing some website matches in the compareresults script because of normalization. This is fixed, and the score goes up a tiny little bit.

--- src/main/resources/compareresults.py
+++ src/main/resources/compareresults.py
@@ -3,13 +3,24 @@ import sys
 import xml.etree.cElementTree as ET
 import collections
 
-# should be kbp2013_test_.....
+print "NOTE: this must be run from the ./src/main/resources/ directory"
+
+if len(sys.argv) < 2:
+    print "Usage: python compareresults.py <gold xml file> [predict file]"
+    sys.exit(0)
+    
 infile = sys.argv[1]
+
+if len(sys.argv) == 3:
+    predict = sys.argv[2]
+else:
+    predict = "../../../data/out/latest/predict.txt"
+
 
 tree = ET.parse(infile)
 root = tree.getroot()
 
-with open("../../../data/out/latest/predict.txt") as f:
+with open(predict) as f:
     predictions = f.readlines()
 
 ans = {}
@@ -19,7 +30,8 @@ tp = 0
 fp = 0
 fn = 0
 tn = 0
-    
+
+# prepare a map from the uschema predictions
 for line in predictions:
     sline = line.strip().split("\t")
     rel = sline[-2]
@@ -47,9 +59,11 @@ for line in predictions:
 d = {}
 mismatches = 0
 
-# gather data
+# for each element in the gold data (eg KBP2013_TESTSET.xml or similar)
 for child in root:
     at = child.attrib
+
+    
     key = "REL$" + at["entity_type"] + ":" + at["attribute"]
     entailment = at["entailment"]
     entity = child[0].text.strip()
@@ -58,6 +72,9 @@ for child in root:
     # key for dict is:
     # rel+entity+value
 
+    # uschema normalizes websites -- so we will too.
+    value = value.replace("/", "").replace("\\", "")
+    
     k = key+":"+entity+":"+value+":"+doc
 
     if k in d and d[k] != entailment:
